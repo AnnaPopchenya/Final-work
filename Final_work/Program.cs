bf6bfdce661e98903a0ed2d6ad410126e5730c5e
Anna <popchenya995@mail.ru>
2023-03-25 20:20:04 +0300

    Добавление массивов и первого метода

--- Final_work/Program.cs
+++ Final_work/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// ЗАДАЧА:  Написать программу, которая из имеющегося массива строк формирует массив строк, длина которых
+// меньше лио равно 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+// При решении  не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+// Примеры. ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
+// ["1234", "1567", "-2", "computer science"] -> ["-2"]
+// ["Russia", "Denmark", "Kazan"] -> []
+
+string[] array1 = new string[4] { "hello", "2", "world", ":-)" };
+string[] array2 = new string[array1.Length];
+
+void SecondArray(string[] arr1, string[] arr2)
+{
+    int i = 0;
+    for (int j = 0; i < arr1.Length; j++)
+    {
+        if (arr1.Length <= 3)
+        {
+            arr2[i] = arr1[j];
+            i++;
+        }
+    }
+}
